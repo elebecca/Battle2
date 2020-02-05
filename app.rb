81fbd7fde1d547d4bc08aa0be2cae74f85cffb56
Student <student@Makerss-MacBook-Air.local>
2020-02-05 15:52:55 +0000

    POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -1,8 +1,28 @@
 require 'sinatra/base'
+require 'sinatra'
+set :session_secret, 'suoper secret'
 
 class Battle < Sinatra::Base
-    get '/' do
-      'Testing infrastructure working!'
-    end
-    run! if app_file == $0
+  enable :sessions
+  # get '/' do
+  #   'Testing infrastructure working!'
+  # end
+  # run! if app_file == $0
+
+  get '/' do
+    erb :index
+  end
+
+  
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name] 
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
+  end
 end
