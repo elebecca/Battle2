6ea3d62473efb85d31f1a9d58f9c357d451dfcc4
Student <student@Makerss-MacBook-Air.local>
2020-02-06 14:24:28 +0000

    Extracting Logic to the Model

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,7 +1,7 @@
 feature 'View hit points' do   
   scenario 'see Player 2 hit points' do
       sign_in_and_play
-      expect(page).to have_content 'Mittens: 10 HP'
+      expect(page).to have_content 'Mittens: 100 HP'
     end
 end
 
