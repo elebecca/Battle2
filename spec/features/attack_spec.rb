8ee7e5ec800456a461ba8d2f9fd87f545c451cad
Student <student@Makerss-MacBook-Air.local>
2020-02-07 12:23:53 +0000

    Implementing hit points

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,14 @@
 feature 'Attacking' do   
     scenario 'Attack Player 2' do
         sign_in_and_play
-        click_button 'attack'
+        click_button 'Attack'
         expect(page).to have_content 'Dave attacked Mittens'
       end
+     scenario 'reduce Player 2 HP by 10' do
+         sign_in_and_play
+         click_button 'Attack'
+         click_link 'OK'
+         expect(page).not_to have_content 'Mittens: 100 HP'
+         expect(page).to have_content 'Mittens: 90 HP'
+    end
   end
