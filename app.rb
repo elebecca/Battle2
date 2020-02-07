b0ab82d0854ae5a5b7b9e098dec64056abf6d382
Student <student@Makerss-MacBook-Air.local>
2020-02-07 14:12:16 +0000

    Single Responsibility Principle

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra/base'
 require 'sinatra'
 require './lib/player.rb'
+require './lib/game.rb'
+
 set :session_secret, 'suoper secret'
 
 class Battle < Sinatra::Base
@@ -29,7 +31,7 @@ class Battle < Sinatra::Base
   get '/attack' do
     @player_1 = $player_1
     @player_2 = $player_2
-    @player_1.attack(@player_2)
+    Game.new.attack(@player_2)
     erb :attack
   end
 
